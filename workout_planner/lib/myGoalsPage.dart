29e303df3dcef4ad894e23ffb2fe6d516f204fff
Shan Khinda <skhinda@mail.csuchico.edu>
2019-02-25 13:40:27 -0800

    Added textfield, enter button, and close button to dialog in goals page.

--- workout_planner/lib/myGoalsPage.dart
+++ workout_planner/lib/myGoalsPage.dart
@@ -4,6 +4,12 @@ import 'main.dart';
 // page to add, track, and edit goals
 class MyGoalsPage extends StatelessWidget
 {
+  final myController = new TextEditingController();
+  var goalText = '';
+
+  void setController(){
+    myController.text = '';
+  }
   @override
   Widget build(BuildContext context)
   {
@@ -16,10 +22,50 @@ class MyGoalsPage extends StatelessWidget
       body: Text("Goals Page"),
 
       floatingActionButton: FloatingActionButton(
-          onPressed:() {},
-          child: Icon(Icons.add)
+          onPressed:() {
+            showDialog(context: context, child:
+              SimpleDialog(
+                title: Text("Enter a goal."),
+                children: <Widget>[
+                  new Column(
+                    children: <Widget>[
+                      new TextField(
+                        controller: myController,
+                        decoration: new InputDecoration(
+                          hintText: "Enter A Goal",
+                          filled: true,
+                          fillColor: Colors.grey[100],
+                          contentPadding: new EdgeInsets.all(5.0)
+                        ),
+                      ),
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget> [
+                          new RaisedButton(
+                            onPressed: () {
+                              goalText = myController.text;
+                              print(myController.text);
+                              myController.clear();
+                            },
+                            child: new Text('Enter'),
+                            color: Colors.blue[100],
+                          ),
+                          new RaisedButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: new Text('Close'),
+                            color: Colors.blue[100],
+                          )
+                        ]
+                      ),
+                    ],
+                  ),
+                ]
+              ),
+            );
+          },
+          child: Icon(Icons.add),
       ),
-
+      
     );
   }
 }
