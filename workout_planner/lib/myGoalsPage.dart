4cec7c63a54a4caa004fedd23c77745434fd6f56
abaker-skudlarek <abaker-skudlarek@mail.csuchico.edu>
2019-02-25 17:12:49 -0800

    Made dialog show up when a goal is clicked, need to make it update goal instead of add goal

--- workout_planner/lib/myGoalsPage.dart
+++ workout_planner/lib/myGoalsPage.dart
@@ -25,30 +25,95 @@ class _MyGoalsPage extends State<MyGoalsPage>
   }
 
   _buildRow(int index){
-    return new Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Column(
+    return new GestureDetector(
+        onTap: (){
+          _showGoalDialog();  //TODO make this UPDATE the goal and description, not change it
+          //_updateGoalDialog(); //TODO this should be the function to update dialog
+        },
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                goals[index],
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold
-                )
+              Column(
+                children: <Widget>[
+                  Text(
+                      goals[index],
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                  Text(
+                      goalDescriptions[index],
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.italic
+                      )
+                  ),
+                ],
               ),
-              Text(
-                goalDescriptions[index],
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontStyle: FontStyle.italic
-                )
+            ],
+          )
+        )
+    );
+  }
+
+  _showGoalDialog()
+  {
+    showDialog(context: context, child:
+      SimpleDialog(
+        title: Text("Enter a goal."),
+        children: <Widget>[
+          new Column(
+            children: <Widget>[
+              new TextField(
+                controller: goalController,
+                decoration: new InputDecoration(
+                    hintText: "Goal",
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                    contentPadding: new EdgeInsets.all(5.0)
+                ),
+              ),
+              new TextField(
+                controller: descController,
+                decoration: new InputDecoration(
+                    hintText: "Description",
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                    contentPadding: new EdgeInsets.all(5.0)
+                ),
+              ),
+              new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget> [
+                    new RaisedButton(
+                      onPressed: () {
+                        _commitGoal();
+                        print(goalController.text);
+                        print(descController.text);
+                        goalController.clear(); // makes sure there isn't leftover text from the last input
+                        descController.clear();
+                        Navigator.pop(context); // closes dialog when enter button is pressed
+                      },
+                      child: new Text('Enter'),
+                      color: Colors.blue[100],
+                    ),
+                    new RaisedButton(
+                      onPressed: () {
+                        goalController.clear(); // makes sure there isn't leftover text from the last input
+                        descController.clear();
+                        Navigator.pop(context);
+                      },
+                      child: new Text('Close'),
+                      color: Colors.blue[100],
+                    )
+                  ]
               ),
             ],
           ),
-        ],
-       )
+        ]
+      ),
     );
   }
 
@@ -62,63 +127,7 @@ class _MyGoalsPage extends State<MyGoalsPage>
       ),
 
       floatingActionButton: FloatingActionButton(
-          onPressed:() {
-            showDialog(context: context, child:
-              SimpleDialog(
-                title: Text("Enter a goal."),
-                children: <Widget>[
-                  new Column(
-                    children: <Widget>[
-                      new TextField(
-                        controller: goalController,
-                        decoration: new InputDecoration(
-                          hintText: "Goal",
-                          filled: true,
-                          fillColor: Colors.grey[100],
-                          contentPadding: new EdgeInsets.all(5.0)
-                        ),
-                      ),
-                      new TextField(
-                        controller: descController,
-                        decoration: new InputDecoration(
-                            hintText: "Description",
-                            filled: true,
-                            fillColor: Colors.grey[100],
-                            contentPadding: new EdgeInsets.all(5.0)
-                        ),
-                      ),
-                      new Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget> [
-                          new RaisedButton(
-                            onPressed: () {
-                              _commitGoal();
-                              print(goalController.text);
-                              print(descController.text);
-                              goalController.clear(); // makes sure there isn't leftover text from the last input
-                              descController.clear();
-                              Navigator.pop(context); // closes dialog when enter button is pressed
-                            },
-                            child: new Text('Enter'),
-                            color: Colors.blue[100],
-                          ),
-                          new RaisedButton(
-                            onPressed: () {
-                              goalController.clear(); // makes sure there isn't leftover text from the last input
-                              descController.clear();
-                              Navigator.pop(context);
-                            },
-                            child: new Text('Close'),
-                            color: Colors.blue[100],
-                          )
-                        ]
-                      ),
-                    ],
-                  ),
-                ]
-              ),
-            );
-          },
+          onPressed:() => _showGoalDialog(),
           child: Icon(Icons.add),
       ),
       body: new Container (
